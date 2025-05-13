9227c146e12ddb4d5bc08c29375d4e7455137242
shazab-saifi <shazebsaifi92@gmail.com>
2025-05-13 14:57:54 +0530

    Completed Button component

---
+++ components/Button.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/lib/utils"
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded-md font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none shadow-md cursor-pointer',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-black text-white hover:bg-black/90',
+        secondary: 'bg-white text-black-foreground hover:bg-white/80',
+      },
+      size: {
+        sm: 'h-9 px-3 text-sm',
+        md: 'h-10 px-4 text-sm',
+        lg: 'h-11 px-8 text-base',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
+    },
+  }
+)
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({children, className, variant, size, ...rest}) => {
+    return <button className={cn(buttonVariants({variant, size}), className)} {...rest}>
+      {children}
+    </button>
+  }
+)
+
+export default Button
---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+import { HandThumbDownIcon } from '@heroicons/react/16/solid'
+
+const Navbar = () => {
+  return (
+    <div className='px-20 py-8'>
+        
+    </div>
+  )
+}
+
+export default Navbar
