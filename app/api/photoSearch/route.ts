8a4b3085eca2c1e647a061b3f41bf180b623eb48
shazab-saifi <shazebsaifi92@gmail.com>
2025-06-14 18:03:37 +0530

    Completed the photoSearch endpoint

--- app/api/photoSearch/route.ts
+++ app/api/photoSearch/route.ts
@@ -1,3 +1,21 @@
-export async function GET() {
-    
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const query = req.nextUrl.searchParams.get('query');
+    const page = req.nextUrl.searchParams.get('page') || '1';
+
+    try {
+        const res = await axios(`https://api.pexels.com/v1/search?query=${query}&per_page=10&page=${page}`, {
+            headers: {
+                Authorization: process.env.PEXELS_API_KEY
+            }
+        });
+
+        return NextResponse.json(res.data);
+    } catch (error) {
+        return new NextResponse(JSON.stringify(error), {
+            status: 500
+        });
+    }
 }
